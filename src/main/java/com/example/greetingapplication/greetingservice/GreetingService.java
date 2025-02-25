df837ac7ed2c86004404bc3d7f43d2cc879c8c02
Code-CLan-Community <veergupta0801@gmail.com>
2025-02-25 12:03:43 +0530

    [Rishabh gupta] Add. UC3 Create Greeting Message

---
+++ src/main/java/com/example/greetingapplication/greetingservice/GreetingService.java
@@ -0,0 +1,28 @@
+package com.example.greetingapplication.greetingservice;
+
+import com.example.greetingapplication.greetingmodel.Greeting;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public Greeting getSimpleGreeting() {
+        return new Greeting(0L, "Hello World");
+    }
+
+    //uc3 create greeting message
+    public Greeting createGreeting(String firstName, String lastName) {
+        String message = "Hello";
+        if (firstName != null && !firstName.isEmpty()) {
+            message += " " + firstName;
+        }
+        if (lastName != null && !lastName.isEmpty()) {
+            message += " " + lastName;
+        }
+        if (firstName == null && lastName == null) {
+            message = "Hello World";
+        }
+        return new Greeting(1L, message);
+    }
+
+}

a00b19f3c5a6bf4f668fb8ca4f841f3562bb0ed7
Code-CLan-Community <veergupta0801@gmail.com>
2025-02-27 12:56:50 +0530

    [Rishabh Gupta] Add. UC5_Find Message BY ID

---
+++ src/main/java/com/example/greetingapplication/greetingservice/GreetingService.java
@@ -0,0 +1,41 @@
+package com.example.greetingapplication.greetingservice;
+
+import com.example.greetingapplication.greetingmodel.Greeting;
+import com.example.greetingapplication.greetingrepository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public Greeting getSimpleGreeting() {
+        return new Greeting(0L, "Hello World");
+    }
+
+    public Greeting createGreeting(String firstName, String lastName) {
+        String message = "Hello";
+        if (firstName != null && !firstName.isEmpty()) {
+            message += " " + firstName;
+        }
+        if (lastName != null && !lastName.isEmpty()) {
+            message += " " + lastName;
+        }
+        if (firstName == null && lastName == null) {
+            message = "Hello World";
+        }
+        return new Greeting(1L, message);
+    }
+
+    //uc4 cREATE A REPOSITORY
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    public Greeting saveGreeting(Greeting greeting) {
+        return greetingRepository.save(greeting);
+    }
+
+    public Greeting findGreetingById(Long id) {
+        return greetingRepository.findById(id).orElse(null);
+    }
+
+}
